e49e8fcd25f3600a0a5c21d67ca59acce5dd2117
RobinHood0101 <robinkammer@proton.me>
2025-09-10 11:41:10 +0200

    add new migrations

---
+++ database/migrations/2025_09_10_063406_create_exercise_plan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exercise_plan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('exercise_id');
+            $table->foreignId('plan_id');
+            $table->integer('weight')->nullable();
+            $table->integer('repetitions')->nullable();
+            $table->integer('sets')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exercise_plan');
+    }
+};
